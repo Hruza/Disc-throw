0078511d9913d9a85f32d2d70b54d2094911f1b3
hlavatyt <tomas7846@gmail.com>
2021-04-05 23:03:04 +0200

    Hyzer throw in UI

--- disk_thow_UI.py
+++ disk_thow_UI.py
@@ -14,42 +14,49 @@ class UI():
     def __init__(self):
         self.app_wind = Tk()
 
-    def __init__(self,v0,angle0,om0):
+    def __init__(self,v0,angle0,om0,hyzangle):
         self.app_wind = Tk()
         self.initv0 = v0
         self.initangle = angle0
         self.initom0 = om0
-        self.solName = '%g_%g_%g.npy'%(float(self.initv0),float(self.initangle),float(self.initom0))
-        self.sol = np.load('./solutions/%s'%self.solName)
+        self.inithyzangle = hyzangle
+        self.solName = '%g_%g_%g_%g.npy'%(float(self.initv0),float(self.initangle),float(self.initom0),float(self.inithyzangle))
+        try:
+            self.sol = np.load('./solutions/%s'%self.solName)
+        except:
+            self.sol = ''
 
     def updateVelGraph(self):
-        self.sol = np.load('./solutions/%s'%self.solName)
-        self.pltVel.clf()
-        # self.pltVel = plt.figure(constrained_layout=True)
-        self.spec2 = gridspec.GridSpec(ncols=2, nrows=2, figure=self.pltVel)
-        self.velPltAx = self.pltVel.add_subplot(self.spec2[0, 0])
-        self.velPltAx.plot(self.sol[-1, :], self.sol[3, :], label='v_x')
-        self.velPltAx.plot(self.sol[-1, :], self.sol[4, :], label='v_y')
-        self.velPltAx.plot(self.sol[-1, :], self.sol[5, :], label='v_z')
-        self.velPltAx.set_xlabel('t')
-        self.velPltAx.set_ylabel('v_i')
-        self.velPltAx.legend()
-        self.velPltAx = self.pltVel.add_subplot(self.spec2[0, 1])
-        self.velPltAx.plot(self.sol[-1, :], self.sol[6, :], label='phi')
-        self.velPltAx.plot(self.sol[-1, :], self.sol[7, :], label='theta')
-        self.velPltAx.plot(self.sol[-1, :], self.sol[8, :], label='psi')
-        self.velPltAx.set_xlabel('t')
-        self.velPltAx.set_ylabel('phi, theta, psi')
-        self.velPltAx.legend()
-        self.velPltAx = self.pltVel.add_subplot(self.spec2[1, 0])
-        self.velPltAx.plot(self.sol[-1, :], self.sol[9, :], label='om1')
-        self.velPltAx.plot(self.sol[-1, :], self.sol[10, :], label='om2')
-        self.velPltAx.plot(self.sol[-1, :], self.sol[11, :], label='om3')
-        self.velPltAx.set_xlabel('t')
-        self.velPltAx.set_ylabel('om_i')
-        self.velPltAx.legend()
-        self.pltVelCan.draw()
-        self.pltVelTB.update()
+        try:
+            self.sol = np.load('./solutions/%s' % self.solName)
+            self.pltVel.clf()
+            # self.pltVel = plt.figure(constrained_layout=True)
+            self.spec2 = gridspec.GridSpec(ncols=2, nrows=2,figure=self.pltVel)
+            self.velPltAx = self.pltVel.add_subplot(self.spec2[0, 0])
+            self.velPltAx.plot(self.sol[-1, :], self.sol[3, :],label='v_x')
+            self.velPltAx.plot(self.sol[-1, :], self.sol[4, :],label='v_y')
+            self.velPltAx.plot(self.sol[-1, :], self.sol[5, :],label='v_z')
+            self.velPltAx.set_xlabel('t')
+            self.velPltAx.set_ylabel('v_i')
+            self.velPltAx.legend()
+            self.velPltAx = self.pltVel.add_subplot(self.spec2[0, 1])
+            self.velPltAx.plot(self.sol[-1, :], self.sol[6, :],label='phi')
+            self.velPltAx.plot(self.sol[-1, :], self.sol[7, :], label='theta')
+            self.velPltAx.plot(self.sol[-1, :], self.sol[8, :],label='psi')
+            self.velPltAx.set_xlabel('t')
+            self.velPltAx.set_ylabel('phi, theta, psi')
+            self.velPltAx.legend()
+            self.velPltAx = self.pltVel.add_subplot(self.spec2[1, 0])
+            self.velPltAx.plot(self.sol[-1, :], self.sol[9, :],label='om1')
+            self.velPltAx.plot(self.sol[-1, :], self.sol[10, :], label='om2')
+            self.velPltAx.plot(self.sol[-1, :], self.sol[11, :],label='om3')
+            self.velPltAx.set_xlabel('t')
+            self.velPltAx.set_ylabel('om_i')
+            self.velPltAx.legend()
+            self.pltVelCan.draw()
+            self.pltVelTB.update()
+        except:
+            pass
 
     def updateTrajGraph(self):
         self.sols = []
@@ -108,15 +115,16 @@ class UI():
         self.v0 = float(self.v0En.get())
         self.angl = float(self.angleEn.get())
         self.init_rotation = float(self.init_rotEn.get())
-        solution = compute(self.v0, self.angl, 0, self.init_rotation)
+        self.hyzer_angle = float(self.hyzer_angleEn.get())
+        solution = compute(self.v0, self.angl, self.hyzer_angle, self.init_rotation)
         self.sol = np.append(solution.y,solution.t.reshape(1,-1),axis=0)
-        self.solName = '%g_%g_%g.npy'%(self.v0, self.angl, self.init_rotation)
+        self.solName = '%g_%g_%g_%g.npy'%(self.v0, self.angl, self.init_rotation,self.hyzer_angle)
         np.save('./solutions/%s'%self.solName,self.sol)
 
     def updateTraj(self):
         self.lsSols = os.listdir('./solutions/')
         for i in range(len(self.solOmTr)):
-            Label(self.traj_fr,text='option menu: v0_theta_omega.npy').grid(row=i,column=0)
+            Label(self.traj_fr,text='option menu: v0_theta_omega_hyz.npy').grid(row=i,column=0)
             self.solOmTr[i].set(self.lsSols[0])
             self.solOmTr[i].trace('w', self.on_opTraj_change)
             self.opTr[i].grid(row=i, column=1)
@@ -161,23 +169,27 @@ class UI():
         Label(self.solver_fr, text='starting velocity').grid(row=3,column=0)
         Label(self.solver_fr, text='angle of the throw').grid(row=4,column=0)
         Label(self.solver_fr, text='init rotation').grid(row=5,column=0)
+        Label(self.solver_fr, text='hazer angle').grid(row=6,column=0)
         self.solver_bt = Button(master=self.solver_fr, width=20,font=('Helvetica', 15), text='Solve',bg='grey', command=self.solverBtClick)
-        self.solver_bt.grid(row=6, column=0,columnspan=2)
+        self.solver_bt.grid(row=8, column=0,columnspan=2)
         # column 1
         self.v0En = Entry(self.solver_fr)
         self.angleEn = Entry(self.solver_fr)
         self.init_rotEn = Entry(self.solver_fr)
+        self.hyzer_angleEn = Entry(self.solver_fr)
         self.v0En.insert(0, self.initv0)
         self.angleEn.insert(0,self.initangle)
         self.init_rotEn.insert(0, self.initom0)
+        self.hyzer_angleEn.insert(0, self.inithyzangle)
         self.v0En.grid(row=3, column=1)
         self.angleEn.grid(row=4, column=1)
         self.init_rotEn.grid(row=5, column=1)
+        self.hyzer_angleEn.grid(row=6, column=1)
         self.solver_fr.pack(fill=BOTH,expand=True)
 
         # trajectory tab
         self.traj_fr = Frame(self.tab3)
-        Label(self.traj_fr,text='option menu: v0_theta_omega.npy').grid(row=0,column=0)
+        Label(self.traj_fr,text='option menu: v0_theta_omega_hyz.npy').grid(row=0,column=0)
         # option list
         self.lsSols = os.listdir('./solutions/')
         self.solOmTr = [StringVar(self.traj_fr)]
@@ -191,7 +203,7 @@ class UI():
         self.solver_bt.grid(row=self.solver_bt_row,column=0)
 
         # graph
-        self.pltTraj = matplotlib.pyplot.figure(figsize=(7, 5), dpi=100)
+        self.pltTraj = matplotlib.pyplot.figure(figsize=(7, 4), dpi=100)
         self.pltTrajCan = FigureCanvasTkAgg(self.pltTraj,master=self.traj_fr)
         self.pltTrajCan.get_tk_widget().grid(row=self.solver_bt_row+1, columnspan=2)
         self.pltTrajTB = NavigationToolbar2Tk(self.pltTrajCan, self.tab3)
@@ -200,7 +212,7 @@ class UI():
 
         # velocitty solutions tab
         self.solution_fr = Frame(self.tab2)
-        Label(self.solution_fr,text='option menu: v0_theta_omega.npy').grid(row=0,column=0)
+        Label(self.solution_fr,text='option menu: v0_theta_omega_hyz.npy').grid(row=0,column=0)
         #option list
         self.lsSols = os.listdir('./solutions/')
         self.solOm = StringVar(self.solution_fr)
@@ -221,7 +233,7 @@ class UI():
         self.app_wind.mainloop()
 
 
-UI_ = UI(10,20,-30)
+UI_ = UI(10,20,-30,0)
 UI_.runUI()
 
 
